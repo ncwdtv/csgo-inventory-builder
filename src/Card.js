8f6a630f638e1f1ad3ac789578506f7aa088ec59
Nick Deupree <nicholasdeupree@gmail.com>
2023-08-10 10:59:40 -0700

    adding card and updating index.css

--- src/Card.js
+++ src/Card.js
@@ -1,13 +1,18 @@
 import PictureElement from "./PictureElement";
 const Card = ({data}) => {
 
-    const { weapon,skin,picture } = data;
-
+    const { skin,picture,pictureLink } = data;
+    
     return ( 
         <div class="card">
             <div class="card-details">
                 <p class="text-title">{skin}</p>
-                <p class="text-body">Here are the details of the card</p>
+                <p class="text-body"><PictureElement
+                                    data={{
+                                        picture: picture,
+                                        pictureLink: steamMarketLink,
+                                    }}
+                                    /></p>
             </div>
             <button class="card-button">More info</button>
         </div>
